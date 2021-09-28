845b149b36381fd1ca97e841990fd02278215420
CodeAkio <siqueira_2004@hotmail.com>
2021-09-28 01:50:53 -0300

    Read and write file

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,6 +12,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _toDoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    var file = File("${directory.path}/data.json");
+
+    return file;
+  }
+
+  Future<File> _saveData() async {
+    var data = json.encode(_toDoList);
+    final file = await _getFile();
+    var updatedFile = file.writeAsString(data);
+
+    return updatedFile;
+  }
+
+  Future<String?> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
